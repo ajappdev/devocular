1c51b34da1596f278a27309ba54239294fec3caa
Alae <alae1ajbar@gmail.com>
2022-06-29 00:57:36 +0200

    add, and update task forms

--- app/views.py
+++ app/views.py
@@ -70,6 +70,26 @@ def roadmap(request, project_id):
     task_form = af.TaskForm(initial={"status": "Open"})
     versions = am.Project.objects.get(id=project_id).versions()
 
+    if request.method == "POST":
+
+        # CREATE NEW TASK
+        new_task = am.Task()
+        new_task.version = am.Version.objects.get(id=request.POST['version'])
+        new_task.task_name = request.POST['task_name']
+        new_task.task_end = request.POST['task_end']
+        new_task.task_description = request.POST['task_description']
+        new_task.priority = request.POST['priority']
+        new_task.status = request.POST['status']
+        new_task.save()
+        
+        # ASSIGN USERS TO THIS TASK
+        users_assigned = request.POST['user_profile']
+        for user in users_assigned:
+            user_profile = am.UserProfile.objects.get(id=user)
+            new_task.user_profile.add(user_profile)
+
+        new_task.save()
+
     template = 'roadmap/project-roadmap.html'
     context = {
         "page_title": "Project roadmap",
